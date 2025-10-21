d3d3d2e793b6692a893cff1ff31328de58d206fd
Andrew Tang <andy@andrewtang.net>
2025-10-21 20:25:05 +1100

    footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -42,7 +42,7 @@ export default function Footer() {
         </div>
 
         <p className="text-sm text-muted dark:text-muted-dark inline-flex items-center gap-1 group">
-          Made with <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block transition-all group-hover:stroke-red-500 group-hover:fill-red-500 group-hover:scale-110" aria-hidden="true"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></svg> in Melbourne, Australia
+          Made with <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block transition-all group-hover:stroke-amber-600 group-hover:scale-110" aria-hidden="true"><path d="M10 2v2"></path><path d="M14 2v2"></path><path d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1"></path><path d="M6 2v2"></path></svg> in Melbourne, Australia
         </p>
       </div>
     </footer>
